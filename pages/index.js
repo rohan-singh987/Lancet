adb7f4e99c00472f52cc859a24afc321831bf7cc
AKASH SINGH <furiousakash111@gmail.com>
2022-03-10 15:33:23 +0530

    One more component added to home page

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,39 @@ export default function Home() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <main>
-        <section className='bg-gray-400'>
+        <section className='bg-gray-100 py-5 md:py-12 lg:py-14'>
+          <div className='bg-gray-400 rounded-lg w-3/4 mx-auto py-5 md:flex lg:hover:scale-105 transition-all ease-in-out lg:rounded-2xl lg:py-3'>
+            <div className='md:w-3/5 md:my-auto lg:w-7/12'>
+              <h2 className='text-center uppercase font-semibold mb-3 sm:text-xl sm:font-bold md:text-left md:px-10 lg:text-2xl xl:text-3xl'>Thriving creativity</h2>
+              <p className='text-center text-sm px-3 sm:px-10 md:text-left lg:text-base lg:pr-14 xl:text-lg'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto doloribus voluptatem quia. Tempore qui iure alias delectus, dignissimos maiores</p>
+            </div>
+            <div className='p-5 mx-auto sm:w-96 md:w-2/5 lg:w-5/12'>
+              <Image
+                src="/media/home_img_2.jpeg"
+                height="415"
+                width="415"
+                alt="Company Logo"
+              />
+            </div>
+          </div>
+        </section>
+        <section className='bg-gray-100 py-5 md:py-12 lg:py-14'>
+          <div className='bg-gray-400 rounded-lg w-3/4 mx-auto py-5 md:flex md:flex-row-reverse lg:hover:scale-105 transition-all ease-in-out lg:rounded-2xl lg:py-3'>
+            <div className='md:w-3/5 md:my-auto lg:w-7/12'>
+              <h2 className='text-center uppercase font-semibold mb-3 sm:text-xl sm:font-bold md:text-right md:px-10 lg:text-2xl xl:text-3xl'>Crafting Dreams</h2>
+              <p className='text-center text-sm px-3 sm:px-10 md:text-right lg:text-base lg:pl-14 xl:text-lg'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto doloribus voluptatem quia. Tempore qui iure alias delectus, dignissimos maiores</p>
+            </div>
+            <div className='p-5 mx-auto sm:w-96 md:w-2/5 lg:w-5/12'>
+              <Image
+                src="/media/home_img_2.jpeg"
+                height="415"
+                width="415"
+                alt="Company Logo"
+              />
+            </div>
+          </div>
+        </section>
+        <section className='bg-gray-400 py-3'>
           <section className='p-3'>
             <h1 className='text-center text-xl font-bold md:text-2xl uppercase lg:text-3xl xl:text-4xl'>Why Us?</h1>
             <article>
